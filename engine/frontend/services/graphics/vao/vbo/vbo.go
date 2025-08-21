4dbd83e21ededabc5a748b106a0c9cf96abd1b32
ogiusek <kowalewski.olgierd@gmail.com>
2025-08-21 12:23:36 +0200

    added comment for vbo for futhure changes

--- engine/frontend/services/graphics/vao/vbo/vbo.go
+++ engine/frontend/services/graphics/vao/vbo/vbo.go
@@ -19,9 +19,13 @@ type vbo struct {
 	len int
 }
 
+// i do not like this coupling
 type Vertex struct {
-	Pos        [3]float32
+	Pos [3]float32
+	// normal [3]float32
 	TexturePos [2]float32
+	// color [4]float32
+	// vertexGroups (for animation) []VertexGroupWeight {Name string; weight float32} (weights should add up to one)
 }
 
 func NewVBO() VBO {

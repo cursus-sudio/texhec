4543823b42435ede52f639aef3ac830ab8c950e9
ogiusek <kowalewski.olgierd@gmail.com>
2025-11-17 21:20:42 +0100

    removed comments

--- core/modules/tile/tile.go
+++ core/modules/tile/tile.go
@@ -1,9 +1,5 @@
 package tile
 
-// per tile we want:
-// - 1-3 textures to draw
-// - 1 button with 1-3 different sets of buttons rendered at once
-
 const (
 	GroundLayer uint8 = iota
 	BuildingLayer
@@ -18,26 +14,3 @@ type PosComponent struct {
 func NewPos(x, y int32, layer uint8) PosComponent {
 	return PosComponent{x, y, layer}
 }
-
-//
-
-// example:
-// 1. soldier def has:
-// - max health
-// 2. soldier has:
-// - unit
-// - pos
-// - health (optional)
-
-// definitions can have:
-// - max hp
-// - cost
-// - recruits
-// - round profit
-// - attacks
-// - defense (optional, armor, immunities etc)
-// -
-
-// attack:
-// - min, max range
-// - damage

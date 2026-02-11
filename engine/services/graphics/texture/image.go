f4e0d2af0e304b841352b6ba9ba8bb9c9322701b
ogiusek <kowalewski.olgierd@gmail.com>
2026-02-11 21:44:31 +0100

    added opaque and scale to images

--- engine/services/graphics/texture/image.go
+++ engine/services/graphics/texture/image.go
@@ -2,7 +2,10 @@ package gtexture
 
 import (
 	"image"
+	"image/color"
 	"image/draw"
+
+	xdraw "golang.org/x/image/draw"
 )
 
 type Image interface {
@@ -17,6 +20,8 @@ type Image interface {
 	RotateClockwise(times int) Image
 
 	TrimTransparentBackground() Image
+	Scale(w, h int) Image
+	Opaque() Image
 }
 
 type img struct {
@@ -32,33 +37,33 @@ func (s *img) Image() image.Image {
 }
 
 func (s *img) FlipH() Image {
-    bounds := s.img.Bounds()
-    newImg := image.NewRGBA(bounds)
+	bounds := s.img.Bounds()
+	newImg := image.NewRGBA(bounds)
 
-    for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-        for x := bounds.Min.X; x < bounds.Max.X; x++ {
-            // Corrected coordinate mapping
-            oldX := bounds.Max.X + bounds.Min.X - x - 1
-            newImg.Set(x, y, s.img.At(oldX, y))
-        }
-    }
-    s.img = newImg
-    return s
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			// Corrected coordinate mapping
+			oldX := bounds.Max.X + bounds.Min.X - x - 1
+			newImg.Set(x, y, s.img.At(oldX, y))
+		}
+	}
+	s.img = newImg
+	return s
 }
 
 func (s *img) FlipV() Image {
-    bounds := s.img.Bounds()
-    newImg := image.NewRGBA(bounds)
+	bounds := s.img.Bounds()
+	newImg := image.NewRGBA(bounds)
 
-    for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-        // Corrected coordinate mapping
-        oldY := bounds.Max.Y + bounds.Min.Y - y - 1
-        for x := bounds.Min.X; x < bounds.Max.X; x++ {
-            newImg.Set(x, y, s.img.At(x, oldY))
-        }
-    }
-    s.img = newImg
-    return s
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		// Corrected coordinate mapping
+		oldY := bounds.Max.Y + bounds.Min.Y - y - 1
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			newImg.Set(x, y, s.img.At(x, oldY))
+		}
+	}
+	s.img = newImg
+	return s
 }
 
 func (s *img) FlipHV() Image {
@@ -118,5 +123,40 @@ func (s *img) TrimTransparentBackground() Image {
 	draw.Draw(croppedImg, croppedImg.Bounds(), s.img, sourcePoint, draw.Src)
 
 	s.img = croppedImg
+	return s
+}
+
+func (s *img) Scale(w, h int) Image {
+	dst := image.NewRGBA(image.Rect(0, 0, w, h))
+	xdraw.BiLinear.Scale(dst, dst.Bounds(), s.img, s.img.Bounds(), draw.Over, nil)
+	s.img = dst
+
+	return s
+}
+
+func (s *img) Opaque() Image {
+	bounds := s.img.Bounds()
+	dst := image.NewRGBA(bounds)
+
+	for y := bounds.Min.Y; y <= bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x <= bounds.Max.X; x++ {
+			c := s.img.At(x, y)
+			r, g, b, a := c.RGBA()
+			if uint16(a) < ^uint16(0)/2 {
+				a = 0
+			} else {
+				a = uint32(^uint16(0))
+			}
+
+			dst.Set(x, y, color.RGBA64{
+				R: uint16(r),
+				G: uint16(g),
+				B: uint16(b),
+				A: uint16(a),
+			})
+		}
+	}
+	s.img = dst
+
 	return s
 }

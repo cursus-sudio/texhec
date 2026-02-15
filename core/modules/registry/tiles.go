b9a67e1a3cf5ee01ec604d68bc10423fb085320d
ogiusek <kowalewski.olgierd@gmail.com>
2026-02-15 17:01:11 +0100

    added comment to current registry

--- core/modules/registry/tiles.go
+++ core/modules/registry/tiles.go
@@ -3,6 +3,9 @@ package registry
 import "core/modules/tile"
 
 const (
+	// this has to be changed before using saving
+	// when i save game and then update to new version which adds new tile as not last
+	// than other tiles are pushed back and every next tile is marked as previous
 	_ tile.ID = iota
 	TileWater
 	TileSand
